ab979b34048b6986768a7f1bbc53c33a43213a9e
ElPapi42 <whitman-2@hotmail.com>
2020-11-23 16:40:46 -0400

    Basic creation of dummy companies

--- seeder/cli.py
+++ seeder/cli.py
@@ -1,14 +1,44 @@
 import typer
-
-from pymongo import MongoClient
+import random
 
 from seeder import settings
-
-MongoClient()
+from seeder.mongo import db
 
 
 app = typer.Typer()
 
+def generate_company():
+    """Generates random data for a dummy company."""
+    companies = [
+        'Ala Delta',
+        'Next Evo',
+        'Dummy File',
+        'Dummy Lol'
+    ]
+
+    addresses = [
+        'Calle 15',
+        'Calle 16',
+        'Calle 17',
+        'Calle 18'
+    ]
+
+    return {
+        'name': random.choice(companies),
+        'nit': random.randint(100000, 999999),
+        'address': random.choice(addresses),
+    }
+
 @app.command()
-def seedusers():
-    typer.echo(f"{settings.DB_URL}")
+def seeddb():
+    # Mongo collection for companies
+    companies = db.companies
+
+    # Insert 3 companies with dummy data
+    new_companies = companies.insert([generate_company() for i in range(3)])
+
+    users = db.users
+
+    for company in new_companies:
+        typer.echo(company)
+
