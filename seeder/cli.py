24aa6667b8fce6fe407aa1c1cdf267ebbd2a3188
ElPapi42 <whitman-2@hotmail.com>
2020-11-23 17:41:32 -0400

    Finish the base script for seed the mongo db

--- seeder/cli.py
+++ seeder/cli.py
@@ -63,10 +63,14 @@ def seeddb():
     companies = db.companies
 
     # Insert 3 companies with dummy data
-    new_companies = companies.insert([generate_company() for i in range(3)])
+    new_companies = companies.insert_many([generate_company() for i in range(3)])
 
+    # collection for users
     users = db.users
 
-    for company in new_companies:
+    for company in new_companies.inserted_ids:
 
-        typer.echo(generate_user(company))
+        # Generate 20 users for each company
+        new_users = users.insert([generate_user(company) for i in range(20)])
+
+        typer.echo(f'Register 20 Users on Company id = {company}')
