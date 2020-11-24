369651877555a8a8d84dd07d2c8bdba30288c4d3
ElPapi42 <whitman-2@hotmail.com>
2020-11-24 08:19:11 -0400

    Add createadmin command

--- seeder/cli.py
+++ seeder/cli.py
@@ -5,7 +5,7 @@ import pymongo
 
 from seeder import settings
 from seeder.mongo import db
-from seeder.users import generate_user
+from seeder.users import generate_user, pass_hasher
 from seeder.companies import generate_company
 
 
@@ -31,3 +31,21 @@ def seeddb():
         new_users = users.insert([generate_user(company) for i in range(20)])
 
         typer.echo(f'Register 20 Users on Company id = {company}')
+
+@app.command()
+def createadmin():
+    admin_exists = db.users.count_documents({'admin': True})
+    if admin_exists:
+        typer.echo('Admin already registered! If you want another admin, ask the admin to give you permissions trough the API')
+        return
+
+    email = typer.prompt('email')
+    password = typer.prompt('password')
+
+    db.users.insert_one({
+        'email': email,
+        'password': pass_hasher.hash(password),
+        'admin': True
+    })
+
+    typer.echo('Seed admin registered!')
